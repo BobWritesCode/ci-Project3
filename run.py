120de7e94a668b8764362c2ac67a25d8ad09d080
BobWritesCode <warwick_hart@hotmail.com>
2022-09-16 16:15:12 +0000

    Validate user input for main menu

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+GAMETITLE = 'Hotdog Tycoon' #Name change pending
+print(f'Preparing to start {GAMETITLE}...\n')
 
 import gspread
 from google.oauth2.service_account import Credentials
@@ -14,13 +16,13 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize (SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Hotdog_Tycoon_Data')
-GAMETITLE = 'Hotdog Tycoon' #Name change pending
 
 
 def get_leaderboard_data():
     '''
     Gets leaderbaord data from Google sheet and displays in terminal
     '''
+    clear_terminal()
     highscore = SHEET.worksheet('leaderboard')
     data = highscore.get_all_values()
     print('\n************************************')
@@ -29,7 +31,7 @@ def get_leaderboard_data():
     print(f"{data[0][0]:<30}{data[0][1]:<40}")
     print('------------------------------------')
     for x in data[1:10]:
-      print(f"{x[0]:<30}{x[1]:<40}")
+        print(f"{x[0]:<30}{x[1]:<40}")
 
 
 def main_menu():
@@ -50,11 +52,44 @@ def main_menu():
     print(f'3. View leaderboard')
     print(f'4. Credits')
     print('------------------------------------')
-    user_choice = input("\nInput choice: ")
+
+
+    while True:
+        user_choice = input("\nInput choice: ")
+        if validate_input(user_choice):
+            print('Choice is valid!')
+            break
+
+
+def validate_input(value):
+    '''
+    Inside the try, converts input string value into integer.
+    Raises ValueError if strings cannot be converted into int,
+    or if outside the expected range.
+    '''
+    try:
+        try:
+            int_value = int(value)
+        except:
+            print(f'Invalid input: please try again.\n')
+            return False
+        if int_value >= 1 and int_value <= 4:
+          return True
+        else:
+            raise ValueError()
+    except ValueError as e:
+        print(f'Invalid input: please try again.\n')
+        return False
+
+    return True
 
 
 def clear_terminal():
+    '''
+    Clears terminal for better user experience
+    '''
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def main():
     '''
@@ -62,6 +97,4 @@ def main():
     '''
     main_menu()
 
-
-print(f'Preparing to start {GAMETITLE}...\n')
 main()
